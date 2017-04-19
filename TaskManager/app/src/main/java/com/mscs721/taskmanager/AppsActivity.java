f4001f609373b2fee157479dce6764556ea4c66d
Mitin Sharma <mitin.sharma1@marist.edu>
2017-04-19 17:55:08 -0400

    List all apps code done
    List of apps (List Apps), View for this class such as all_apps.xml need to update to work with this class

--- TaskManager/app/src/main/java/com/mscs721/taskmanager/AppsActivity.java
+++ TaskManager/app/src/main/java/com/mscs721/taskmanager/AppsActivity.java
@@ -22,6 +22,21 @@ public class AppsActivity extends Activity {
         List<ApplicationInfo> apps = pm.getInstalledApplications(0);
 
         new AlertDialog.Builder(AppsActivity.this).setMessage(apps.toString()).show();
+        
+        lv = (ListView) findViewById(R.id.your_list_view_id);
+
+         // Instanciating an array list (you don't need to do this, 
+         // you already have yours).
+         List<String> array_list = new ArrayList<String>();
+         array_list.add("App Name");
+         //array_list.add("bar");
+
+         // This is the array adapter, it takes the context of the activity as a 
+         // first parameter, the type of list view as a second parameter and your 
+         // array as a third parameter.
+         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.list_apps,array_list );
+
+         lv.setAdapter(arrayAdapter); 
 
     }
 }
