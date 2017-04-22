b36357c37e87c084f733dc7204d4361290f07e5b
Vishal <vishal.koosuri1@marist.edu>
2017-04-21 22:35:10 -0400

    Able to view graph

--- TaskManager/app/src/main/java/com/mscs721/taskmanager/MainActivity.java
+++ TaskManager/app/src/main/java/com/mscs721/taskmanager/MainActivity.java
@@ -6,30 +6,52 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.mscs721.taskmanager.AppsActivity;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mFreeMemMessage;
     private TextView mTotalMemMessage, cpuTxt;
     private Button allButton, appsButton, systemButton;
+    private PieChart pieChart;
+    private ArrayList<Entry> entries;
+    private ArrayList<String> labels;
+    private PieDataSet pieDataSet;
+    private PieData pieData;
+    private long availableMem=0;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        pieChart = (PieChart) findViewById(R.id.chart1);
+        entries = new ArrayList<>();
+        labels = new ArrayList<String>();
+        addValuesToPieChart();
+        addValuesToLabels();
+
+        pieDataSet = new PieDataSet(entries, "");
+        pieData = new PieData(labels, pieDataSet);
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        pieChart.setData(pieData);
+
         ActivityManager.MemoryInfo mi = new ActivityManager.MemoryInfo();
         ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
         activityManager.getMemoryInfo(mi);
         long totalMemory = mi.totalMem/1048576L;
-        long availableMem = mi.availMem/1048576L;
+        availableMem = mi.availMem/1048576L;
 
         mFreeMemMessage = (TextView) findViewById(R.id.freeMemory);
         mTotalMemMessage = (TextView) findViewById(R.id.totalMemory);
@@ -70,7 +92,19 @@ public class MainActivity extends AppCompatActivity {
         cpuTxt.setText("CPU usage: "+readUsage());
     }
 
-    private float readUsage() {
+    public void addValuesToPieChart() {
+        entries.add(new BarEntry(2f, (int) readUsage()));
+        entries.add(new BarEntry(8f, (int)availableMem));
+        entries.add(new BarEntry(6f, 2));
+    }
+
+    public void addValuesToLabels() {
+        labels.add("CPU Usage");
+        labels.add("Available Memory");
+        labels.add("Random");
+    }
+
+    public float readUsage() {
         try {
             RandomAccessFile reader = new RandomAccessFile("/proc/stat", "r");
             String load = reader.readLine();
@@ -104,4 +138,7 @@ public class MainActivity extends AppCompatActivity {
         return 0;
     }
 
+    public int CpuRead(){
+        return 1;
+    }
 }
