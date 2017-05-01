d4d38f2e72a3dd1909d8b2981de0285a14a8770c
Vishal <vishal.koosuri1@marist.edu>
2017-05-01 18:29:57 -0400

    Minor fixes and updates

--- TaskManager/app/src/main/java/com/mscs721/taskmanager/AllAppsActivity.java
+++ TaskManager/app/src/main/java/com/mscs721/taskmanager/AllAppsActivity.java
@@ -9,8 +9,11 @@
 package com.mscs721.taskmanager;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -18,42 +21,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class AllAppsActivity extends Activity{
+public class AllAppsActivity extends Activity {
 
     private ListView lv;
+    private ArrayAdapter<String> arrayAdapter;
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        //Application is not loaded this method is called
         super.onCreate(savedInstanceState);
         setContentView(R.layout.all_apps);
-        
-        // Setting Up the title 
-        setTitle("All Apps");
-        lv = (ListView) findViewById(R.id.listview1);
 
-        // Creating a list of strings
-        List<String> installedApps = getInstalledApps();
-        ArrayAdapter<String> arrayAdapter =
-                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, installedApps);
-        
-        // Setting the data behind the list view
+        setTitle("System Apps");
+        lv = (ListView) findViewById(R.id.listview1);
+        final List<String> installedApps = getInstalledApps();
+
+        // Creating new adapter to store the data
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, installedApps );
+
+        // Setting the data behind the list
         lv.setAdapter(arrayAdapter);
+
+        // Callback is invoked when an item in this AdapterView has been clicked.
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Toast.makeText(getApplicationContext(), "Click ListItem Number " + position + ", id: "+id, Toast.LENGTH_SHORT).show();
+                String s = arrayAdapter.getItem(position);
+                Intent in = new Intent(getApplicationContext(), AppDetail.class);
+                in.putExtra("app", s);
+                //Toast.makeText(getApplicationContext(), "S: " + s, Toast.LENGTH_SHORT).show();
+                startActivity(in);
+            }
+        });
     }
+
+
+
     private List<String> getInstalledApps() {
         List<String> res = new ArrayList<String>();
         List<PackageInfo> packs = getPackageManager().getInstalledPackages(0);
         for (int i = 0; i < packs.size(); i++) {
             PackageInfo p = packs.get(i);
-            if (true) {
                 String appName = p.applicationInfo.loadLabel(getPackageManager()).toString();
+                String packName = p.packageName.toString();
                 //Drawable icon = p.applicationInfo.loadIcon(getPackageManager());
                 //res.add(new AppList(appName, icon));
-                res.add(appName);
-            }
+                res.add(packName);
         }
         return res;
     }
-
-//    private boolean isSystemPackage(PackageInfo pkgInfo) {
-//        return ((pkgInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0);
-//    }
 }
--- TaskManager/app/src/main/java/com/mscs721/taskmanager/SystemAppsActivity.java
+++ TaskManager/app/src/main/java/com/mscs721/taskmanager/SystemAppsActivity.java
@@ -17,7 +17,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +24,7 @@ import java.util.List;
 public class SystemAppsActivity extends Activity {
 
     private ListView lv;
+    private ArrayAdapter<String> arrayAdapter;
     @Override
     // It intialize our activity
     public void onCreate(Bundle savedInstanceState) {
@@ -34,43 +34,47 @@ public class SystemAppsActivity extends Activity {
 
         setTitle("System Apps");
         lv = (ListView) findViewById(R.id.listview1);
-        List<String> installedApps = getInstalledApps();
-        
+        final List<String> installedApps = getInstalledApps();
+
         // Creating new adapter to store the data
-        final ArrayAdapter<String> arrayAdapter =
-                new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,installedApps );
-        
-        // Setiing the data behind the list
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, installedApps );
+
+        // Setting the data behind the list
         lv.setAdapter(arrayAdapter);
-        
+
         // Callback is invoked when an item in this AdapterView has been clicked.
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent;
-                //sposition = 0;
-                Toast.makeText(getApplicationContext(), "Click ListItem Number " + position, Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(), "Click ListItem Number " + position + ", id: "+id, Toast.LENGTH_SHORT).show();
+                String s = arrayAdapter.getItem(position);
+                Intent in = new Intent(getApplicationContext(), AppDetail.class);
+                in.putExtra("app", s);
+                //Toast.makeText(getApplicationContext(), "S: " + s, Toast.LENGTH_SHORT).show();
+                startActivity(in);
             }
         });
     }
-    
+
+
     // Returns the List of all apps installed
     private List<String> getInstalledApps() {
         List<String> res = new ArrayList<String>();
         List<PackageInfo> packs = getPackageManager().getInstalledPackages(0);
         for (int i = 0; i < packs.size(); i++) {
             PackageInfo p = packs.get(i);
-            if ((isSystemPackage(p) == true)) {
+            if (isSystemPackage(p)) {
                 String appName = p.applicationInfo.loadLabel(getPackageManager()).toString();
+                String packName = p.packageName.toString();
                 //Drawable icon = p.applicationInfo.loadIcon(getPackageManager());
                 //res.add(new AppList(appName, icon));
-                res.add(new String(appName));
+                res.add(packName);
             }
         }
         return res;
     }
     // Filter the system app
     private boolean isSystemPackage(PackageInfo pkgInfo) {
-        return ((pkgInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0) ? true : false;
+        return ((pkgInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0);
     }
 }
